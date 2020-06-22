26c4e76002265675bd7013f87ea872e252d8b544
Mark Alldritt <alldritt@latenightsw.com>
2020-06-21 19:16:50 -0700

    Initial Commit

---
+++ IconEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IconEditor
+//
+//  Created by Mark Alldritt on 2020-06-21.
+//  Copyright © 2020 Mark Alldritt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
